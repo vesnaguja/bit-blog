c5d0db7b5b379c50e26d448c318f58da278326fb
vesnaguja <80161546+vesnaguja@users.noreply.github.com>
2021-10-06 14:09:12 +0200

    fix: logo link operator

--- src/app/shared/Header.jsx
+++ src/app/shared/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <Navbar bg="light" expand="lg" className="fixed-top mb-5">
       <Container>
-        <Navbar.Brand href={"/" || "/bit-blog"}>BIT-BLOG</Navbar.Brand>
+        <Navbar.Brand href={"/" && "/bit-blog"}>BIT-BLOG</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav>
